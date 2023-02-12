61d1f8aceb5fb74455087730a73308525c63ec00
Jordan Primas <jorprimas20@gmail.com>
2023-02-12 07:34:57 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) {
+        return block - 42 
+    } else if (42 > block) {
+        return 42 - block
+    }
+}
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * 264
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination - start) *264
+    } else {
+        return (start - destination) *264
+    }
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const distanceTraveled = distanceTravelledInFeet(start, destination)
+
+    if (distanceTraveled <= 400) {
+        return 0
+    } 
+    else if (distanceTraveled > 400 && distanceTraveled <= 2000 ) { 
+        return (distanceTraveled - 400) * .02
+    }
+    else if (distanceTraveled > 2000 && distanceTraveled <= 2500) {
+        return 2500 / 100
+    }
+    else {
+        return 'cannot travel that far'
+    }
+} 
